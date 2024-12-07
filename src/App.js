7ce091f8dbc0e7e8362edb5a660b82d7288361ab
adon <adon@ops8888.com>
2024-12-08 00:59:40 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-expressions */
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+					test--facebook web2app
+				</a>
+			</header>
+		</div>
+	);
+}
+
+export default App;
